afb583ef16865ed626e87d93676619de7fd11cfc
ragomez33 <raggmagm@gmail.com>
2024-04-18 16:52:40 -0400

    fix icons in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Col, Row } from 'antd';
 import styled from '@emotion/styled';
-import { Flex } from "antd";
 import { Image } from "antd";
-import GmailIcon from '../assets/icons8-gmail-48.png';
+import GmailIcon from '../assets/icons8-gmail-nuevo-50.png';
 import GithubIcon from '../assets/github-mark-white.png';
+import LinkedinIcon from '../assets/icons8-linkedin-50.png';
 import { Typography } from "antd";
 
 const HeaderContainer = styled.div`
@@ -15,26 +15,40 @@ const IconContainer = styled.div`
 width: 50px;
 height: 50px;
 `;
-
+const IconContainerRadius = styled.div`
+width: 42px;
+height: 42px;
+border-radius: 50px;
+border: solid 1px #fff;
+background-color: #fff;
+padding: 4px;
+`;
 export const Header: React.FC = () => {
   return (
     <HeaderContainer>
-      <Row>
-        <Col span={3} offset={21} xs={6} md={4}>
-          <Flex justify={'space-around'}>
-            <IconContainer>
-              <a href="mailto:raggmagm@gmail.com" title='' target='_blank' rel="noreferrer">
-                <Image src={GmailIcon} preview={false} />
-              </a>
-            </IconContainer>
-            <IconContainer>
-              <a href="https://github.com/Ragomez33" title='' target='_blank' rel="noreferrer">
-                <Image src={GithubIcon} preview={false} />
-              </a>
-            </IconContainer>
-          </Flex>
+      <Row justify='end'>
+        <Col xs={5} sm={3} md={3} xl={2}>
+          <IconContainerRadius>
+            <a href="mailto:raggmagm@gmail.com" title='' target='_blank' rel="noreferrer">
+              <Image src={GmailIcon} preview={false} />
+            </a>
+          </IconContainerRadius>
         </Col>
-        <Col>
+        <Col xs={5} sm={3} md={3} xl={2}>
+          <IconContainer>
+            <a href="https://github.com/Ragomez33" title='' target='_blank' rel="noreferrer">
+              <Image src={GithubIcon} preview={false} />
+            </a>
+          </IconContainer>
+        </Col>
+        <Col xs={5} sm={3} md={3} xl={2}>
+          <IconContainerRadius>
+            <a href="https://github.com/Ragomez33" title='' target='_blank' rel="noreferrer">
+              <Image src={LinkedinIcon} preview={false} />
+            </a>
+          </IconContainerRadius>
+        </Col>
+        <Col xs={24}>
           <Typography.Title
             style={{
               color: '#d1d1d1',
